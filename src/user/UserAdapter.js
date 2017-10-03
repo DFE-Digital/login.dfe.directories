eebd047749c790a2916b01580e52cb23aec5b05e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-03 08:00:39 +0100

    Implement authenticate on base adapter
    Added the authenticate to the base adapter. This will be the same for all adapters.

--- src/user/UserAdapter.js
+++ src/user/UserAdapter.js
@@ -1,11 +1,31 @@
 'use strict';
+const crypto = require('crypto');
+const {promisify} = require('util');
+const RequestVerification = require('login.dfe.request-verification');
+
+let requestVerification;
 
 class UserAdapter {
+  constructor(){
+    requestVerification = new RequestVerification();
+  }
   async find(id) {
     return Promise.resolve({});
   }
-  async authenticate(email, password, sig) {
-    return Promise.resolve({});
+  async authenticate(username, password, sig) {
+    const contents = JSON.stringify({username: username, password: password});
+    if (requestVerification.verifyRequest(contents, config.RequestVerificationCertification, sig)) {
+      const user = this.find(username);
+
+      const request = promisify(crypto.pbkdf2);
+
+      const derivedKey = request(password, user.salt, 100000, 512, 'sha512');
+
+      if (derivedKey.toString('hex') === user.password) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 

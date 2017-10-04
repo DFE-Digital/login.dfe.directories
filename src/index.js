13daed8ffc6547bf3bbfd9feb49cc19c7fb49dcc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-04 15:02:16 +0100

    Remove api from route

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use('/api',api());
+app.use('/',api(config));
 
 if (config.hostingEnvironment.env === 'dev') {
   app.proxy = true;

d54dd25066089062655a2f2eb7156a7148e3c9ad
Simon Bull <simon.bull.84@googlemail.com>
2017-10-05 16:15:19 +0100

    Create user model and update local file to match
    * Create user model that uses standard OIDC claim names.
    * Update local file to match format of user model

---
+++ src/user/UserModel.js
@@ -0,0 +1,26 @@
+class UserModel {
+
+  get sub() {
+    return this.sub;
+  }
+  set sub(value) {
+    this.sub = value;
+  }
+  
+  get given_name() {
+    return this.given_name;
+  }
+  set given_name(value) {
+    this.given_name = value;
+  }
+
+  get family_name() {
+    return this.family_name;
+  }
+  set family_name(value) {
+    this.family_name = value;
+  }
+  
+}
+
+module.exports = UserModel;

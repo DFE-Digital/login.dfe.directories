20f5baeab810cf16661105a0fff44139ff704a56
Simon Bull <simon.bull.84@googlemail.com>
2017-10-06 09:41:33 +0100

    Update tests

--- src/user/UserModel.js
+++ src/user/UserModel.js
@@ -1,31 +1,31 @@
 class UserModel {
 
   get sub() {
-    return this.sub;
+    return this._sub;
   }
   set sub(value) {
-    this.sub = value;
+    this._sub = value;
   }
   
   get given_name() {
-    return this.given_name;
+    return this._given_name;
   }
   set given_name(value) {
-    this.given_name = value;
+    this._given_name = value;
   }
 
   get family_name() {
-    return this.family_name;
+    return this._family_name;
   }
   set family_name(value) {
-    this.family_name = value;
+    this._family_name = value;
   }
 
   get email() {
-    return this.email;
+    return this._email;
   }
   set email(value) {
-    this.email = value;
+    this._email = value;
   }
   
 }

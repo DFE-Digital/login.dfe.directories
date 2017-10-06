ed415ced9cb3a3dfc37c992fe824894a476942ba
Simon Bull <simon.bull.84@googlemail.com>
2017-10-06 08:36:49 +0100

    Update Redis adapter for new structure

--- src/user/UserRedisAdapter.js
+++ src/user/UserRedisAdapter.js
@@ -6,30 +6,48 @@ const redis = require('ioredis');
 let client;
 let configuration;
 
-class UserRedisAdapter extends UserAdapter{
-  constructor(redisClient,config){
+class UserRedisAdapter extends UserAdapter {
+  constructor(redisClient, config) {
     super();
     configuration = config;
-    if(redisClient === null || redisClient === undefined){
+    if (redisClient === null || redisClient === undefined) {
       client = new redis(configuration.redisurl);
-    } else{
+    } else {
       client = redisClient;
     }
   }
 
   async find(id) {
-
     return new Promise((resolve, reject) => {
       client.get('Users').then((result) => {
-        if(result === null || result === undefined){
+        if (result === null || result === undefined) {
           resolve(null);
         }
         const users = JSON.parse(result);
 
-        if(users === null || users === undefined){
+        if (users === null || users === undefined) {
           resolve(null);
-        }else{
+        } else {
           const user = users.find((item) => item.sub === id);
+
+          resolve(user === undefined ? null : user);
+        }
+      });
+    });
+  }
+
+  async findByUsername(username) {
+    return new Promise((resolve, reject) => {
+      client.get('Users').then((result) => {
+        if (result === null || result === undefined) {
+          resolve(null);
+        }
+        const users = JSON.parse(result);
+
+        if (users === null || users === undefined) {
+          resolve(null);
+        } else {
+          const user = users.find((item) => item.email === username);
 
           resolve(user === undefined ? null : user);
         }

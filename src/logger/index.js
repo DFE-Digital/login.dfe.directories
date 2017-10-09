c39a7197a2db44e1dd06fe70c6b791b15ae0c75b
Floyd Price <f.price@floydprice.com>
2017-10-09 18:15:45 +0100

    Add Logger refactored api to have seperate modules

---
+++ src/logger/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const winston = require('winston');
+const config = require('./../config');
+
+const logLevel = (config && config.loggerSettings && config.loggerSettings.logLevel) ? config.loggerSettings.logLevel : 'info';
+
+const logger = new (winston.Logger)({
+  colors: (config && config.loggerSettings && config.loggerSettings.colors) ? config.loggerSettings.colors : null,
+  transports: [
+    new (winston.transports.Console)({ level: logLevel, colorize: true }),
+  ],
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  logger.error('Unhandled Rejection at:', p, 'reason:', reason);
+});
+
+module.exports = logger;

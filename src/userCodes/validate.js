b7a3b0ebfa3d9cd7deb59526dc95134e37082b2d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-13 13:20:49 +0100

    Add validate endpoint
    Added endpoint to return a response if the uid and code supplied match what is stored.

---
+++ src/userCodes/validate.js
@@ -0,0 +1,36 @@
+'use strict';
+const logger = require('../logger');
+const redisUserCodeStorage = require('./redisUserCodeStorage');
+
+const validate = async (req,res) => {
+  try{
+    if(req.params.uid === null || req.params.uid === undefined || req.params.uid === ''
+      || req.params.code === null || req.params.code === undefined || req.params.code === '') {
+      res.status(400).send();
+      return;
+    }
+    const uid = req.params.uid;
+    const storage = new redisUserCodeStorage();
+
+    storage.getUserPasswordResetCode(uid).then((code) => {
+      storage.close();
+      if(code === null || code === undefined){
+        res.status(404).send();
+        return;
+      }
+
+      if(code.code === req.params.code) {
+        res.status(200).send(code);
+        return;
+      }
+      res.status(404).send();
+    });
+  } catch (e) {
+    logger.error(e);
+    res.status(500).send(e);
+  }
+
+
+};
+
+module.exports = validate;

964b273b0fe290683bd1f8507192363ada719f77
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-20 10:18:53 +0100

    Eslint fixes

--- src/utils/generateCode.js
+++ src/utils/generateCode.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const DEC_CHARSSET = 'ABCDEFHJKLMNPRSTUV';
+const DEC_CHARSET = 'ABCDEFHJKLMNPRSTUV';
 const FULL_CHARSET = 'ABCDEFGHJKMNPQRSTWXYZabcdefghjkmnpqrstwxyz23456789-.><!@%&*+_';
 
-const generate = (length = 8, charset = DEC_CHARSSET) => {
+const generate = (length = 8, charset = DEC_CHARSET) => {
   let secret = '';
   for(let i = 0; i < length; i++) {
     secret += charset[Math.floor(Math.random() * charset.length)];
@@ -12,7 +12,7 @@ const generate = (length = 8, charset = DEC_CHARSSET) => {
 };
 
 module.exports = {
-  DEC_CHARSSET,
+  DEC_CHARSET,
   FULL_CHARSET,
   generate
 };

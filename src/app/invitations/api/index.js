d05f06d451dd12dbbee639b52127dc3a568eaddc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-11-10 10:13:57 +0000

    Add config entry for invitations
    Added config entry for the redisurl for invitations, and also added assert check to make sure it has been populated

--- src/app/invitations/api/index.js
+++ src/app/invitations/api/index.js
@@ -5,11 +5,15 @@ const apiAuth = require('login.dfe.api.auth');
 const config = require('./../../../infrastructure/config')();
 const putUpsertInvitation = require('./putUpsertInvitation');
 
+const assert = require('assert');
+
 const router = express.Router();
 
 const routeExport = () => {
   // Add auth middleware.
   router.use(apiAuth(router, config));
+
+  assert(config.invitations.redisUrl, 'the invitations.redisUrl config property must be set');
 
   // Map routed to functions.
   router.put('/upsert', putUpsertInvitation);

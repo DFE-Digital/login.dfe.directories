d12a99fc9353361412e55fca379dd901898316f1
Floyd Price <f.price@floydprice.com>
2017-11-16 22:01:02 +0000

    test for correct signiture on UserRedisAdaptor.create

--- test/usertests/UserRedisAdapter.test.js
+++ test/usertests/UserRedisAdapter.test.js
@@ -1,8 +1,10 @@
+'use strict';
+
 const RedisMock = require('ioredis-mock').default;
 const UserStorage = require('./../../src/app/user/adapter/UserRedisAdapter');
-const { promisify } = require('util');
-const crypto = require('crypto');
+const {promisify} = require('util');
 
+const crypto = require('crypto');
 describe('When using redis storage service', () => {
   describe('then when I call find', () => {
     let redis;
@@ -75,5 +77,28 @@ describe('When using redis storage service', () => {
       const derivedKey = await request('my-new-password', saltBuffer, 10000, 512, 'sha512');
       expect(findResult.password).toBe(derivedKey.toString('base64'));
     });
+  });
+  describe('then when i call create', () => {
+    let redis;
+    let userStorage;
+
+    beforeEach(() => {
+      redis = new RedisMock();
+      redis.disconnect = () => true;
+      userStorage = new UserStorage(redis);
+    });
+
+    it('null is returned if no username and/or password is specified', async () => {
+
+      const username = null;
+      const password = null;
+      const firstName = null;
+      const lastName = null;
+
+      const actual = await userStorage.create(username, password, firstName, lastName);
+
+      expect(actual).toBeNull();
+    });
+
   });
 });

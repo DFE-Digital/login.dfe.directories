29a63f15cbc071441c8f30aed83cbf5f96b56486
Floyd Price <f.price@floydprice.com>
2017-11-16 18:38:16 +0000

    deprecate old route style (where /:directory_id/ was required) using a new deprecation middleware

--- src/app/user/api/index.js
+++ src/app/user/api/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const apiAuth = require('login.dfe.api.auth');
 const config = require('./../../../infrastructure/config')();
+const {deprecate} = require('./../../../utils');
 const authenticate = require('./authenticate');
 const changePassword = require('./changePassword');
 const find = require('./find');
@@ -14,9 +15,13 @@ const routeExport = () => {
   router.use('/', apiAuth(router, config));
 
   // Map routed to functions.
-  router.get('/:directoryId/user/:id', find);
-  router.post('/:directoryId/user/authenticate', authenticate);
-  router.post('/:directoryId/user/:id/changepassword', changePassword);
+  router.get('/:directoryId/user/:id', deprecate('/users/:id'), find);
+  router.post('/:directoryId/user/authenticate', deprecate('/users/authenticate'), authenticate);
+  router.post('/:directoryId/user/:id/changepassword', deprecate('/users/:id/changepassword'), changePassword);
+
+  router.get('/users/:id', find);
+  router.post('/users/authenticate', authenticate);
+  router.post('/users/:id/changepassword', changePassword);
 
   return router;
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const generate = require('./generateCode');
+const deprecate = require('./deprecateMiddleware');
 
 module.exports = {
   DEC_CHARSET: generate.DEC_CHARSET,
   FULL_CHARSET: generate.FULL_CHARSET,
-  generate: generate.generate
+  generate: generate.generate,
+  deprecate,
 };

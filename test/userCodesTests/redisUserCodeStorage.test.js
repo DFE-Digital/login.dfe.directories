7766fc14015acc4ae823ba5f21c3c0dae7cc31a8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-12-07 20:39:26 +0000

    Add redirectUri to be stored against the userCode

--- test/userCodesTests/redisUserCodeStorage.test.js
+++ test/userCodesTests/redisUserCodeStorage.test.js
@@ -12,15 +12,11 @@ describe('When using redis user code storage', () => {
 
   beforeEach(() => {
     generateResetCode = require('./../../src/app/userCodes/utils/generateResetCode');
-    generateResetCode.mockImplementation(() => {
-      return 'ABC123'
-    });
+    generateResetCode.mockImplementation(() => 'ABC123');
 
-    configStub = jest.fn().mockImplementation(() => {
-      return {
-        userCodes: {staticCode: false}
-      };
-    });
+    configStub = jest.fn().mockImplementation(() => ({
+      userCodes: { staticCode: false },
+    }));
 
     config = require('./../../src/infrastructure/config');
     config.mockImplementation(configStub);
@@ -29,14 +25,13 @@ describe('When using redis user code storage', () => {
     userStorage = new userCodeStorage(redis);
   });
   describe('then when I call GetUserPasswordResetCode', () => {
-
     it('then null is returned if the code does not exist for the user', async () => {
       const actual = await userStorage.getUserPasswordResetCode('123');
 
       expect(actual).toBeNull();
     });
     it('then the code is returned with the userid if it exists', async () => {
-      redis.set('UserResetCode_123','{"uid":"123","code":"ABC123"}');
+      redis.set('UserResetCode_123', '{"uid":"123","code":"ABC123"}');
 
       const actual = await userStorage.getUserPasswordResetCode('123');
 
@@ -46,8 +41,7 @@ describe('When using redis user code storage', () => {
   });
   describe('then when i call createUserPasswordResetCode', () => {
     it('then the uid is used to create the record and properties stored against the record', async () => {
-
-      await userStorage.createUserPasswordResetCode('321', 'client1');
+      await userStorage.createUserPasswordResetCode('321', 'client1', 'http://local.test');
       const record = await redis.get('UserResetCode_321');
 
       expect(record).not.toBeNull();
@@ -55,6 +49,7 @@ describe('When using redis user code storage', () => {
       expect(resetCode.clientId).toBe('client1');
       expect(resetCode.uid).toBe('321');
       expect(resetCode.code).toBe('ABC123');
+      expect(resetCode.redirectUri).toBe('http://local.test');
     });
     it('then if the uid is not supplied then a record is not created', async () => {
       const actual = await userStorage.createUserPasswordResetCode();
@@ -62,49 +57,43 @@ describe('When using redis user code storage', () => {
       expect(actual).toBeNull();
     });
     it('then the code is set from the code generation tool', async () => {
-      const actual = await userStorage.createUserPasswordResetCode('321', 'client1');
+      const actual = await userStorage.createUserPasswordResetCode('321', 'client1', 'http://local.test');
 
       expect(actual.code).toBe('ABC123');
     });
     it('then a static code is returned if the config is set', async () => {
       jest.resetModules();
       generateResetCode = require('./../../src/app/userCodes/utils/generateResetCode');
-      generateResetCode.mockImplementation(() => {
-        return 'XYZ123'
-      });
+      generateResetCode.mockImplementation(() => 'XYZ123');
 
       config = require('./../../src/infrastructure/config');
-      configStub = jest.fn().mockImplementation(() => {
-        return {
-          userCodes: {staticCode: true}
-        };
-      });
+      configStub = jest.fn().mockImplementation(() => ({
+        userCodes: { staticCode: true },
+      }));
 
       config.mockImplementation(configStub);
-      
-      const actual = await userStorage.createUserPasswordResetCode('321', 'client1');
+
+      const actual = await userStorage.createUserPasswordResetCode('321', 'client1', 'http://local.test');
 
       expect(actual.code).toBe('ABC123');
     });
   });
   describe('then when i call deleteUserPasswordResetCode', () => {
-
     it('then if the uid is not supplied the record is not deleted', async () => {
-      redis.set('UserResetCode_123','{"uid":"123","code":"ABC123"}');
+      redis.set('UserResetCode_123', '{"uid":"123","code":"ABC123"}');
 
       await userStorage.deleteUserPasswordResetCode();
 
-      var record = await redis.get('UserResetCode_123');
+      const record = await redis.get('UserResetCode_123');
       expect(record).not.toBeNull();
     });
-    it('then the uid is used to find the record and delete it', async ()=> {
-      redis.set('UserResetCode_123','{"uid":"123","code":"ABC123"}');
+    it('then the uid is used to find the record and delete it', async () => {
+      redis.set('UserResetCode_123', '{"uid":"123","code":"ABC123"}');
 
       await userStorage.deleteUserPasswordResetCode('123');
 
-      var record = await redis.get('UserResetCode_123');
+      const record = await redis.get('UserResetCode_123');
       expect(record).toBeNull();
     });
-
   });
 });

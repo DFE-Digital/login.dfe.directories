76737c6c3773bfa704d7446235f43b30863ad380
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-12-22 08:38:17 +0000

    eslint fixes

--- src/app/user/devices/static.js
+++ src/app/user/devices/static.js
@@ -1,16 +1,12 @@
-const getUserDevices = async (userId) => {
-  return [
-    {
-      id: 'e716d7d8-5d32-4cd0-9daf-43a03fd4ed04',
-      type: 'digipass',
-      serialNumber: '123456',
-    },
-  ];
-};
+const getUserDevices = async userId => [
+  {
+    id: 'e716d7d8-5d32-4cd0-9daf-43a03fd4ed04',
+    type: 'digipass',
+    serialNumber: '123456',
+  },
+];
 
-const createUserDevices = async (userId, device) => {
-  return Promise.resolve(null);
-};
+const createUserDevices = async (userId, device) => Promise.resolve(null);
 
 module.exports = {
   getUserDevices,

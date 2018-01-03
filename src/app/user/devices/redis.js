27a0f22fcff65e32b89141f6d88de95c9a98267d
Simon Bull <simon.bull.84@googlemail.com>
2018-01-03 11:59:33 +0000

    Log correlationid in custom data

--- src/app/user/devices/redis.js
+++ src/app/user/devices/redis.js
@@ -6,27 +6,27 @@ const redis = new IORedis(config.devices.redisUrl);
 
 const getUserDevices = async (userId, correlationId) => {
   try {
-    logger.info(`Get user devices for request: ${correlationId}`);
+    logger.info(`Get user devices for request: ${correlationId}`, { correlationId });
     const json = await redis.get(`UserDevices_${userId}`);
     if (!json) {
       return [];
     }
     return JSON.parse(json);
   } catch (e) {
-    logger.error(`Get user devices failed for request ${correlationId} error: ${e}`);
+    logger.error(`Get user devices failed for request ${correlationId} error: ${e}`, { correlationId });
     throw (e);
   }
 };
 
 const createUserDevices = async (userId, device, correlationId) => {
   try {
-    logger.info(`Create user devices for request: ${correlationId}`);
+    logger.info(`Create user devices for request: ${correlationId}`, { correlationId });
     const devices = await getUserDevices(userId);
     devices.push(device);
 
     await redis.set(`UserDevices_${userId}`, JSON.stringify(devices));
   } catch (e) {
-    logger.error(`Create user devices failed for request ${correlationId} error: ${e}`);
+    logger.error(`Create user devices failed for request ${correlationId} error: ${e}`, { correlationId });
     throw (e);
   }
 };

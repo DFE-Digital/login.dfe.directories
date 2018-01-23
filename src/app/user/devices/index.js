e29b978c5c70523eb3b726de15a1bea38e9983cb
Simon Bull <simon.bull.84@googlemail.com>
2018-01-23 08:54:10 +0000

    Add azure blob storage

--- src/app/user/devices/index.js
+++ src/app/user/devices/index.js
@@ -3,6 +3,8 @@ const config = require('./../../../infrastructure/config');
 let adapter;
 if (config.devices.type === 'redis') {
   adapter = require('./redis');
+} else if (config.devices.type === 'azureblob') {
+  adapter = require('./azureBlob');
 } else {
   adapter = require('./static');
 }

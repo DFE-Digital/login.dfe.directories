ae67c8b83a93bc86c86bf494453978391398c98b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-01-24 10:14:32 +0000

    Add static and sequelize user_code adapaters

---
+++ src/app/userCodes/data/index.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const config = require('./../../../infrastructure/config');
+const logger = require('./../../../infrastructure/logger');
+
+const getUserCodeAdapter = () => {
+  const userCodesAdapter = config.userCodes;
+  if (userCodesAdapter === null || userCodesAdapter === undefined) {
+    return null;
+  }
+
+  if (userCodesAdapter.type === 'sequelize') {
+    return require('./sequelizeUserCodeStorage');
+  }
+
+  if (userCodesAdapter.type === 'static') {
+    logger.warn('Static code generation being used - codes are not stored');
+    return require('./staticUserCodeStorage');
+  }
+  if (userCodesAdapter.type === 'redis') {
+    assert(userCodesAdapter.params.redisUrl, 'redisUrl is required');
+    return require('./redisUserCodeStorage');
+  }
+
+  return null;
+};
+
+module.exports = getUserCodeAdapter();

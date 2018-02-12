6f97b5918e923b5ad15597a1d6234061ea13469a
Simon Bull <simon.bull.84@googlemail.com>
2018-02-12 12:42:38 +0000

    Get user associated to device

---
+++ src/app/devices/data/redis.js
@@ -0,0 +1,81 @@
+const config = require('./../../../infrastructure/config');
+const logger = require('./../../../infrastructure/logger');
+const IORedis = require('ioredis');
+
+const redis = new IORedis(config.devices.redisUrl);
+
+const readBatchOfKeys = async ({ match = '*', count = 0 }) => {
+  return new Promise((resolve, reject) => {
+    const keys = [];
+    const opts = {
+      match,
+    };
+
+    if (count > 0) {
+      opts.count = count;
+    }
+
+    redis.scanStream(opts)
+      .on('data', (batch) => {
+        for (let i = 0; i < batch.length; i += 1) {
+          keys.push(batch[i]);
+        }
+      })
+      .on('end', () => {
+        resolve(keys);
+      })
+      .on('error', reject);
+  });
+};
+
+const getUserDevices = async (userId, correlationId) => {
+  try {
+    logger.info(`Get user devices for request: ${correlationId}`, { correlationId });
+    const json = await redis.get(`UserDevices_${userId}`);
+    if (!json) {
+      return [];
+    }
+    return JSON.parse(json);
+  } catch (e) {
+    logger.error(`Get user devices failed for request ${correlationId} error: ${e}`, { correlationId });
+    throw (e);
+  }
+};
+
+const createUserDevices = async (userId, device, correlationId) => {
+  try {
+    logger.info(`Create user devices for request: ${correlationId}`, { correlationId });
+    const devices = await getUserDevices(userId);
+    devices.push(device);
+
+    await redis.set(`UserDevices_${userId}`, JSON.stringify(devices));
+  } catch (e) {
+    logger.error(`Create user devices failed for request ${correlationId} error: ${e}`, { correlationId });
+    throw (e);
+  }
+};
+
+const getUserAssociatedToDevice = async (type, serialNumber, correlationId) => {
+  try {
+    logger.info(`Get user associated to device for request: ${correlationId}`, { correlationId });
+
+    const allKeys = await readBatchOfKeys({ match: 'UserDevices_*' });
+    for (let i = 0; i < allKeys.length; i += 1) {
+      const userId = allKeys[i].substr(12);
+      const userDevices = await getUserDevices(userId, correlationId);
+      if (userDevices && userDevices.find(d => d.type.toLowerCase() === type.toLowerCase() && d.serialNumber === serialNumber)) {
+        return userId;
+      }
+    }
+    return null;
+  } catch (e) {
+    logger.error(`Get user devices failed for request ${correlationId} error: ${e}`, { correlationId });
+    throw (e);
+  }
+};
+
+module.exports = {
+  getUserDevices,
+  createUserDevices,
+  getUserAssociatedToDevice,
+};

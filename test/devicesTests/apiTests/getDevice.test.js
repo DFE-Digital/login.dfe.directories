bb2f4f4ab3f13164fc6b10de7f8c87783f586af0
Simon Bull <simon.bull.84@googlemail.com>
2018-02-14 15:17:13 +0000

    Check for device against invitations as well as users

--- test/devicesTests/apiTests/getDevice.test.js
+++ test/devicesTests/apiTests/getDevice.test.js
@@ -3,9 +3,15 @@ jest.mock('./../../../src/app/devices/data', () => {
     getUserAssociatedToDevice: jest.fn(),
   };
 });
+jest.mock('./../../../src/app/invitations/data/redisInvitationStorage', () => {
+  return {
+    list: jest.fn(),
+  };
+});
 
 const httpMocks = require('node-mocks-http');
 const { getUserAssociatedToDevice } = require('./../../../src/app/devices/data');
+const { list } = require('./../../../src/app/invitations/data/redisInvitationStorage');
 const getDevice = require('./../../../src/app/devices/api/getDevice');
 
 describe('When getting user associated with device', () => {
@@ -24,6 +30,20 @@ describe('When getting user associated with device', () => {
 
     getUserAssociatedToDevice.mockReset();
     getUserAssociatedToDevice.mockReturnValue('user-1');
+
+    list.mockReset();
+    list.mockReturnValue({
+      invitations: [{
+        firstName: 'User',
+        lastName: 'One',
+        email: 'user.one@invited.test',
+        keyToSuccessId: '12345678901',
+        tokenSerialNumber: '123456789',
+        id: 'invite-1',
+      }],
+      page: 1,
+      numberOfPages: 1,
+    });
   });
 
   it('then it should get associated user from store', async () => {
@@ -41,14 +61,36 @@ describe('When getting user associated with device', () => {
     expect(res._isJSON()).toBe(true);
     expect(res._getData()).toMatchObject({
       associatedWith: {
+        type: 'user',
         sub: 'user-1',
       },
     });
     expect(res._isEndCalled()).toBe(true);
   });
 
-  it('then it should return 404 if user not found', async () => {
+  it('then it should return invitation if no associated user but invitation', async () => {
     getUserAssociatedToDevice.mockReturnValue(null);
+
+    await getDevice(req, res);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._isJSON()).toBe(true);
+    expect(res._getData()).toMatchObject({
+      associatedWith: {
+        type: 'invitation',
+        sub: 'invite-1',
+      },
+    });
+    expect(res._isEndCalled()).toBe(true);
+  });
+
+  it('then it should return 404 if user not invitation not found', async () => {
+    getUserAssociatedToDevice.mockReturnValue(null);
+    list.mockReturnValue({
+      invitations: [],
+      page: 1,
+      numberOfPages: 1,
+    });
 
     await getDevice(req, res);
 

8128dd49dc700eb6584ca3f763630d86ec91a3ff
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-11 13:21:44 +0100

    Change data type naming

--- database_scripts/sql/1_user_code_column_script.sql
+++ database_scripts/sql/1_user_code_column_script.sql
@@ -1,13 +1,20 @@
 
-IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'user_code' and COLUMN_NAME = 'email')
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'user_code' and COLUMN_NAME = 'email')
 BEGIN
     ALTER TABLE [dbo].[user_code] ADD email varchar(255) null
 END
 GO
 
-IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'user_code' and COLUMN_NAME = 'context_data')
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'user_code' and COLUMN_NAME = 'codeType')
 BEGIN
-    ALTER TABLE [dbo].[user_code] ADD context_data varchar(5000) null
+    ALTER TABLE [dbo].[user_code] ADD codeType varchar(255) not null default 'PasswordReset'
+END
+GO
+
+
+IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'user_code' and COLUMN_NAME = 'contextData')
+BEGIN
+    ALTER TABLE [dbo].[user_code] ADD contextData varchar(5000) null
 END
 GO
 
@@ -15,3 +22,17 @@ IF NOT EXISTS(select 1 from sys.indexes where [name] = 'idx_user_code_email' AND
 BEGIN
     CREATE INDEX idx_user_code_email on  user_code (email)
 END
+
+
+/* manual step
+
+select CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+where TABLE_NAME ='user_code'
+and CONSTRAINT_TYPE = 'PRIMARY KEY'
+
+alter table user_code drop CONSTRAINT <constraint_name>
+
+alter table user_code add primary key (uid,codeType)
+
+*/

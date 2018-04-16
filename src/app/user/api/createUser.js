c0444a5e13162a006cb8085e42b8c32a21fcd837
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-16 11:36:28 +0100

    Add createUser action

---
+++ src/app/user/api/createUser.js
@@ -0,0 +1,27 @@
+const { findByUsername, create } = require('./../adapter');
+const logger = require('./../../../infrastructure/logger');
+const { safeUser } = require('./../../../utils');
+
+const createUser = async (req, res) => {
+  try {
+    if (!req.body.email || !req.body.password || !req.body.firstName || !req.body.lastName) {
+      return res.status(400).send();
+    }
+
+    const existingUser = await findByUsername(req.body.email, req.header('x-correlation-id'));
+
+    if (existingUser) {
+      return res.status(409).send();
+    }
+
+    const user = await create(req.body.email, req.body.password, req.body.firstName, req.body.lastName, req.header('x-correlation-id'));
+
+    return res.send(safeUser(user));
+  } catch (e) {
+    logger.error(e);
+    res.status(500).send(e);
+  }
+};
+
+module.exports = createUser;
+

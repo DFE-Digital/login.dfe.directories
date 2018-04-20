cd588df9d46a681f3446876234f582e6298ecb75
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-04-20 10:46:10 +0100

    Correct table creation script

--- database_scripts/sql/2_user_legacy_username_table_script.sql
+++ database_scripts/sql/2_user_legacy_username_table_script.sql
@@ -1,12 +1,12 @@
-
 IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'user_legacy_username')
 BEGIN
     CREATE TABLE [dbo].[user_legacy_username]
     (
-        uid uniqueidentifier PRIMARY KEY NOT NULL,
-        legacy_username PRIMARY KEY VARCHAR(255) NOT NULL,
+        uid uniqueidentifier NOT NULL,
+        legacy_username VARCHAR(255) NOT NULL,
         createdAt datetime2 NOT NULL,
-        updatedAt datetime2 NOT NULL
+        updatedAt datetime2 NOT NULL,
+        PRIMARY KEY (uid, legacy_username)
     )
 END
 GO

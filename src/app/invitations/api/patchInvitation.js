319bb45ca9c93e3bdf0f9558217042ed2ab48006
Simon Bull <simon.bull.84@googlemail.com>
2018-05-05 08:51:37 +0100

    Allow email to be updated on invitation
    Allow email to be patched on invitation. If changed then it will generate new code and send invitation again

--- src/app/invitations/api/patchInvitation.js
+++ src/app/invitations/api/patchInvitation.js
@@ -1,6 +1,10 @@
+const config = require('./../../../infrastructure/config');
 const { getUserInvitation, updateInvitation } = require('./../data/redisInvitationStorage');
+const { generateInvitationCode } = require('./../utils');
+const NotificationClient = require('login.dfe.notifications.client');
+const { getOidcClientById } = require('./../../../infrastructure/hotConfig');
 
-const patchableProperties = ['isCompleted', 'deactivated', 'reason'];
+const patchableProperties = ['email', 'isCompleted', 'deactivated', 'reason'];
 const patchablePropertiesMessage = patchableProperties.concat();
 
 const validatePatchProperties = (req) => {
@@ -19,6 +23,23 @@ const validatePatchProperties = (req) => {
     return propertyError;
   }
 };
+const sendInvitation = async (invitation) => {
+  const notificationClient = new NotificationClient({
+    connectionString: config.notifications.connectionString,
+  });
+
+  const client = invitation.origin ? await getOidcClientById(invitation.origin.clientId) : undefined;
+  let friendlyName;
+  let digipassRequired = false;
+  if (client) {
+    friendlyName = client.friendlyName;
+    digipassRequired = client.params ? client.params.digipassRequired : false;
+  }
+
+  await notificationClient.sendInvitation(
+    invitation.email, invitation.firstName, invitation.lastName, invitation.id, invitation.code,
+    friendlyName, digipassRequired, invitation.selfStarted);
+};
 
 const patchInvitation = async (req, res) => {
   const invitation = await getUserInvitation(req.params.id, req.header('x-correlation-id'));
@@ -31,8 +52,15 @@ const patchInvitation = async (req, res) => {
     return res.status(400).send(requestError);
   }
 
-  const patchedInvitation = Object.assign(invitation, req.body);
+  const patchedInvitation = Object.assign(Object.assign({}, invitation), req.body);
+  const emailHasChanged = patchedInvitation.email.toLowerCase() !== invitation.email.toLowerCase();
+  if (emailHasChanged) {
+    patchedInvitation.code = generateInvitationCode();
+  }
   await updateInvitation(patchedInvitation);
+  if (emailHasChanged) {
+    await sendInvitation(patchedInvitation);
+  }
 
   return res.status(202).send();
 };

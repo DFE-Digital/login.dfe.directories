02bb2145f0d632e35aeff85f5be8467b7af915bc
Simon Bull <simon.bull.84@googlemail.com>
2018-06-25 14:16:02 +0100

    Add phone number to user

--- src/app/user/api/patchUser.js
+++ src/app/user/api/patchUser.js
@@ -1,7 +1,7 @@
 const { find, update } = require('./../adapter');
 const { safeUser } = require('./../../../utils');
 
-const allowablePatchProperties = ['given_name', 'family_name', 'email'];
+const allowablePatchProperties = ['given_name', 'family_name', 'email', 'phone_number'];
 const allowablePropertiesMessage = allowablePatchProperties.concat();
 
 const validateRequestData = (req) => {
@@ -36,7 +36,7 @@ const patchUser = async (req, res) => {
 
   // Patch user
   const updatedUser = Object.assign(userModel, req.body);
-  await update(updatedUser.sub, updatedUser.given_name, updatedUser.family_name, updatedUser.email, req.header('x-correlation-id'));
+  await update(updatedUser.sub, updatedUser.given_name, updatedUser.family_name, updatedUser.email, updatedUser.phone_number, req.header('x-correlation-id'));
 
   return res.status(202).send();
 };

eb5d080899314c1c18170872b08bd5879cc86aee
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-07-12 16:03:21 +0100

    NSA-1455- resent invitation

--- src/app/invitations/api/postResendInvitation.js
+++ src/app/invitations/api/postResendInvitation.js
@@ -2,10 +2,11 @@
 
 const logger = require('./../../../infrastructure/logger');
 const sendInvitation = require('./../utils/sendInvitation');
+const storage = require('./../data/redisInvitationStorage');
 
 const post = async (req, res) => {
   try {
-    const invitation = await (req.params.id, req.header('x-correlation-id'));
+    const invitation = await storage.getUserInvitation(req.params.id, req.header('x-correlation-id'));
     if (!invitation) {
       return res.status(400).send();
     }

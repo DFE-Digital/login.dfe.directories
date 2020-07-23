3b9b549599bcb1f91f20bf67891fc7612b9561a6
Sumanta Sarkar <sumanta.sr@gmail.com>
2020-07-23 09:22:44 +0100

    added try catch for slack service for azure ssh (#216)
    Co-authored-by: Sumanta <>

--- src/services/slackService.js
+++ src/services/slackService.js
@@ -4,14 +4,19 @@ const config = require('../infrastructure/config');
 class SlackService {
 
     async postMessage(message) {
-        let url = config.notifications.slackWebHookUrl;
-        const webhook = new IncomingWebhook(url);
 
-        await (async () => {
-            await webhook.send({
-                text: message
-            });
-        })();
+        try {
+            let url = config.notifications.slackWebHookUrl;
+            const webhook = new IncomingWebhook(url);
+
+            await (async () => {
+                await webhook.send({
+                    text: message
+                });
+            })();
+        } catch (ex) {
+            console.log("Exception in sending slack message", ex);
+        }
     }
 }
 

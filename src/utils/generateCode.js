d63354dcab5ab1b213c2788e17d6416066c858e6
jo-joy <jovin.joy@outlook.com>
2021-08-06 15:13:24 +0100

    IT healthcheck crypto

--- src/utils/generateCode.js
+++ src/utils/generateCode.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const crypto = require('crypto');
+
 const NUMERIC_CHARSET = '123456789';
 const DEC_CHARSET = '46789BCDFGHJKLMNPRSTVWXY';
 const FULL_CHARSET = 'ABCDEFGHJKMNPQRSTWXYZabcdefghjkmnpqrstwxyz23456789-.><!@%&*+_';
 
+const getRandom = () => {
+  const value = crypto.randomBytes(4).readUInt32LE() / 0x100000000;
+  return value;
+};
+
 const generate = (length = 8, charset = DEC_CHARSET) => {
   let secret = '';
   for (let i = 0; i < length; i += 1) {
-    secret += charset[Math.floor(Math.random() * charset.length)];
+    secret += charset[Math.floor(getRandom() * charset.length)];
   }
   return secret;
 };

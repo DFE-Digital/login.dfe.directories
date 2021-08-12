e490402177537951a85aa4fb09856f22c20bddad
jo-joy <jovin.joy@outlook.com>
2021-08-12 15:39:46 +0100

    updating comment for future referrence

--- src/app/userCodes/utils/generateResetCode.js
+++ src/app/userCodes/utils/generateResetCode.js
@@ -3,3 +3,6 @@
 const { generate, DEC_CHARSET } = require('./../../../utils');
 
 module.exports = () => generate(8, DEC_CHARSET);
+
+
+// The generate function calls the geneerateCode.js to get random set using the new crypto function

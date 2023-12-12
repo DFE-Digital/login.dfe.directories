379ffd8040d082785d08157c0ed2d87f20e8e573
stottidfe <peter.stott@education.gov.uk>
2023-12-12 16:12:10 +0000

    picking uppasword

--- src/app/user/api/matchedPassphrase.js
+++ src/app/user/api/matchedPassphrase.js
@@ -3,7 +3,11 @@ const logger = require('../../../infrastructure/logger');
 
 const matchedPassphrase = async (req, res) => {
   try {
-    const results = await userAdapter.isMatched(req.params.uid, req.body.passphrase, req.header('x-correlation-id'));
+    if(req.body.newPass === undefined || req.body.newPass === '')
+    {
+        return res.status(404).send();
+    }
+    const results = await userAdapter.isMatched(req.params.uid, req.body.newPass, req.header('x-correlation-id'));
     if (!results) {
       return res.status(404).send();
     }

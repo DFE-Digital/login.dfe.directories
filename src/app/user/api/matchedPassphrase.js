63fbe198e8588d156c8ff4a48a51280b129ca091
stottidfe <peter.stott@education.gov.uk>
2023-12-12 15:11:59 +0000

    fix change reset

--- src/app/user/api/matchedPassphrase.js
+++ src/app/user/api/matchedPassphrase.js
@@ -3,7 +3,7 @@ const logger = require('../../../infrastructure/logger');
 
 const matchedPassphrase = async (req, res) => {
   try {
-    const results = await userAdapter.IsMatched(req.params.uid, req.body.passphrase, req.header('x-correlation-id'));
+    const results = await userAdapter.isMatched(req.params.uid, req.body.passphrase, req.header('x-correlation-id'));
     if (!results) {
       return res.status(404).send();
     }

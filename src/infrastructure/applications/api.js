199c90595bfdb5ec5800e024330f5d2fb7632afe
Richard Ward <richard6.ward@education.gov.uk>
2024-05-23 11:58:47 +0100

    Remove login.dfe.request-promise-retry and use login.dfe.async-retry

--- src/infrastructure/applications/api.js
+++ src/infrastructure/applications/api.js
@@ -1,6 +1,6 @@
 const config = require('./../config');
 
-const rp = require('login.dfe.request-promise-retry');
+const { fetchApi } = require('login.dfe.async-retry');
 const jwtStrategy = require('login.dfe.jwt-strategies');
 
 const getServiceById = async (id) => {
@@ -9,13 +9,11 @@ const getServiceById = async (id) => {
   }
   const token = await jwtStrategy(config.applications.service).getBearerToken();
   try {
-    const client = await rp({
+    const client = await fetchApi(`${config.applications.service.url}/services/${id}`, {
       method: 'GET',
-      uri: `${config.applications.service.url}/services/${id}`,
       headers: {
         authorization: `bearer ${token}`,
-      },
-      json: true,
+      }
     });
     return client;
   } catch (e) {

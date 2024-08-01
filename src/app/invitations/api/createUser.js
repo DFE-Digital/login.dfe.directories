2bb1eac64099ed7a73f216d63f5747da736da74d
Ionut Predoi <ionut.predoi@education.gov.uk>
2024-08-01 12:28:52 +0100

    Added functionality to create users when entraOid is provided

--- src/app/invitations/api/createUser.js
+++ src/app/invitations/api/createUser.js
@@ -12,14 +12,13 @@ const ServiceNotificationsClient = require('login.dfe.service-notifications.jobs
 const createUser = async (req, res) => {
   try {
     const invId = req.params.id;
-    const password = req.body.password;
+    const { password, entraOid } = req.body;
 
     if (!invId) {
       return res.status(400).send();
     }
-
-    if (!password) {
-      return res.status(400).send();
+    if ((password && entraOid) || (!password && !entraOid)) {
+      return res.status(400).send({ message: 'Provide either password or entraOid, but not both or neither' });
     }
 
     const invitation = await getUserInvitation(req.params.id, req.header('x-correlation-id'));
@@ -27,7 +26,7 @@ const createUser = async (req, res) => {
       return res.status(404).send();
     }
 
-    const user = await userStorage.create(invitation.email, password, invitation.firstName, invitation.lastName, null, null, req.header('x-correlation-id'), invitation.isMigrated);
+    const user = await userStorage.create(invitation.email, password, invitation.firstName, invitation.lastName, null, null, req.header('x-correlation-id'), invitation.isMigrated, entraOid);
 
     const completedInvitation = Object.assign(invitation, { isCompleted: true, userId: user.id });
     await updateInvitation(completedInvitation);
@@ -37,11 +36,6 @@ const createUser = async (req, res) => {
     });
     await serviceNotificationsClient.notifyUserUpdated(safeUser(user));
 
-    /* const notificationClient = new NotificationClient({
-      connectionString: config.notifications.connectionString,
-    });
-    notificationClient.sendRegistrationComplete(user.email, user.given_name, user.family_name); */
-
     const publicApiClient = new PublicApiClient({
       connectionString: config.notifications.connectionString,
     });
@@ -50,7 +44,7 @@ const createUser = async (req, res) => {
     return res.status(201).send(safeUser(user));
   } catch (e) {
     logger.error(e);
-    res.status(500).send(e.message);
+    return res.status(500).send(e.message);
   }
 };
 

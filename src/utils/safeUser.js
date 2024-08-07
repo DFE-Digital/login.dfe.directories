19a25eb6e6190beea52d88a40bd9e5a30fa18fe8
Ionut Predoi <ionut.predoi@education.gov.uk>
2024-08-07 16:15:04 +0100

    update `safeUser` function to transform entra properties

--- src/utils/safeUser.js
+++ src/utils/safeUser.js
@@ -1,7 +1,35 @@
+/* eslint-disable camelcase */
 'use strict';
 
 const { pick } = require('lodash');
 
-const safeUser = user => pick(user, ['sub', 'given_name', 'family_name', 'email', 'job_title', 'id', 'status', 'legacy_username', 'phone_number', 'last_login', 'prev_login', 'isMigrated', 'password_reset_required', 'is_entra', 'entra_oid', 'entra_linked']);
+const safeUser = (user) => {
+  const safeUserFields = [
+    'sub', 'given_name',
+    'family_name',
+    'email',
+    'job_title',
+    'id',
+    'status',
+    'legacy_username',
+    'phone_number',
+    'last_login',
+    'prev_login',
+    'is_entra',
+    'entra_oid',
+    'entra_linked',
+  ];
+  const rawSafeUser = pick(user, safeUserFields);
+  const {
+    is_entra, entra_oid, entra_linked, ...rest
+  } = rawSafeUser;
+
+  return {
+    ...rest,
+    isEntra: rawSafeUser.is_entra,
+    entraOid: rawSafeUser.entra_oid,
+    entraLinked: rawSafeUser.entra_linked,
+  };
+};
 
 module.exports = safeUser;

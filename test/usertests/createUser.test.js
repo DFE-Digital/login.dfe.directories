a8cef43c7fd54e27ac136a5c38cd2f615c0fb261
Richard Ward <richard.ward@methods.co.uk>
2024-08-14 13:45:17 +0100

    Added functionality and tests when creating a user with an entraOid

--- test/usertests/createUser.test.js
+++ test/usertests/createUser.test.js
@@ -2,12 +2,15 @@ jest.mock('login.dfe.service-notifications.jobs.client');
 jest.mock('./../../src/app/user/adapter', () => ({
   findByUsername: jest.fn(),
   create: jest.fn(),
+  findByEntraOid: jest.fn(),
 }));
+
 jest.mock('./../../src/infrastructure/logger', () => {
   return {
     error: jest.fn(),
     info: jest.fn(),};
 });
+
 jest.mock('./../../src/infrastructure/config', () => {
   return {
     notifications: {
@@ -20,15 +23,17 @@ jest.mock('./../../src/infrastructure/config', () => {
 });
 
 const ServiceNotificationsClient = require('login.dfe.service-notifications.jobs.client');
-const { findByUsername, create } = require('./../../src/app/user/adapter');
-const createUser = require('./../../src/app/user/api/createUser');
+const { findByUsername, create, findByEntraOid } = require('../../src/app/user/adapter');
+const createUser = require('../../src/app/user/api/createUser');
 const httpMocks = require('node-mocks-http');
+const logger = require('../../src/infrastructure/logger');
 
 const newUser = {
   sub: 'some-new-id',
   password: 'somepassword',
   status: 1,
 };
+
 const serviceNotificationsClient = {
   notifyUserUpdated: jest.fn(),
 };
@@ -66,9 +71,11 @@ describe('When creating a user', () => {
     serviceNotificationsClient.notifyUserUpdated.mockReset();
     ServiceNotificationsClient.mockReset().mockImplementation(() => serviceNotificationsClient);
   });
+
   afterEach(() => {
     expect(res._isEndCalled()).toBe(true);
   });
+
   it('then if the params are missing a bad request is returned', async () => {
     req.body.email = '';
 
@@ -76,6 +83,7 @@ describe('When creating a user', () => {
 
     expect(res.statusCode).toBe(400);
   });
+
   it('then if a user already exists with that user a conflict error is returned', async () => {
     findByUsername.mockReturnValue({ email: 'some@local' });
 
@@ -86,6 +94,7 @@ describe('When creating a user', () => {
     expect(findByUsername.mock.calls[0][1]).toBe(expectedRequestCorrelationId);
     expect(res.statusCode).toBe(409);
   });
+
   it('then if the request is valid then the call is made to the repository to create the user', async () => {
     await createUser(req, res);
 
@@ -97,6 +106,7 @@ describe('When creating a user', () => {
     expect(create.mock.calls[0][5]).toBe(req.body.phone_number);
     expect(create.mock.calls[0][6]).toBe(expectedRequestCorrelationId);
   });
+
   it('then if the legacy_username is provided in the body it is passed to the repository', async () => {
     req.body.legacy_username = '123asd';
 
@@ -105,12 +115,14 @@ describe('When creating a user', () => {
     expect(create.mock.calls).toHaveLength(1);
     expect(create.mock.calls[0][4]).toBe(req.body.legacy_username);
   });
+
   it('then the user is returned in the response', async () => {
     await createUser(req, res);
 
     expect(res._getData().sub).toBe('some-new-id');
     expect(res._getData().password).toBe(undefined);
   });
+
   it('then a user updated notification is sent', async () => {
     await createUser(req, res);
 
@@ -118,5 +130,83 @@ describe('When creating a user', () => {
     expect(ServiceNotificationsClient).toHaveBeenCalledWith({ connectionString: 'notifications-connection-string' });
     expect(serviceNotificationsClient.notifyUserUpdated).toHaveBeenCalledTimes(1);
     expect(serviceNotificationsClient.notifyUserUpdated).toHaveBeenCalledWith(Object.assign({}, newUser, { status: newUser.status, password: undefined }));
+  });
+
+  it('should return 400 if no firstName is provided', async () => {
+    req.body.firstName = null;
+
+    await createUser(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should return 400 if no lastName is provided', async () => {
+    req.body.lastName = null;
+
+    await createUser(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should return 400 if the request contains both password and entraOid', async () => {
+    req.body.entraOid = '01913788-ab6a-705e-b861-de8a60edfdb8';
+
+    await createUser(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should return 400 if no password or entraOid is provided', async () => {
+    req.body.entraOid = null;
+    req.body.password = null;
+
+    await createUser(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it('should return 409 if an existing user has been assigned the entraOid', async () => {
+    req.body.password = null;
+    req.body.entraOid = '01913788-ab6a-705e-b861-de8a60edfdb8';
+
+    findByEntraOid.mockReturnValue({ sub: '8b6141a6-294c-4bde-ba9b-2e6e68c87af8', entra_oid: '01913788-ab6a-705e-b861-de8a60edfdb8' });
+
+    await createUser(req, res);
+
+    expect(logger.error).toHaveBeenCalled();
+    expect(res.statusCode).toBe(409);
+  });
+
+  it('should return 500 in the event of an exception being thrown', async () => {
+    req.body.password = null;
+    req.body.entraOid = '01913788-ab6a-705e-b861-de8a60edfdb8';
+
+    findByEntraOid.mockImplementation(() => { throw new Error(); });
+
+    await createUser(req, res);
+
+    expect(res.statusCode).toBe(500);
+  });
+
+  it('should create a user assigned to entraOid', async () => {
+    req.body.password = null;
+    req.body.entraOid = 'd0c342aa-549f-4992-ae00-e8fdc47592a9';
+
+    findByEntraOid.mockReturnValue(undefined);
+
+    await createUser(req, res);
+
+    expect(create.mock.calls).toHaveLength(1);
+    expect(create.mock.calls[0]).toEqual([
+      req.body.email,
+      null,
+      req.body.firstName,
+      req.body.lastName,
+      undefined,
+      req.body.phone_number,
+      expectedRequestCorrelationId,
+      false,
+      'd0c342aa-549f-4992-ae00-e8fdc47592a9',
+    ]);
   });
 });

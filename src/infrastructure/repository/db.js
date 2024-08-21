6053e8af052080897d9bcb715756ce7f6b2e856f
Lea Hayes <lea.hayes@education.gov.uk>
2024-08-21 16:04:59 +0100

    Remove unused devices data model.

--- src/infrastructure/repository/db.js
+++ src/infrastructure/repository/db.js
@@ -10,18 +10,8 @@ module.exports = db = {};
 
 initialize();
 
-const getIntValueOrDefault = (value, defaultValue = 0) => {
-  if (!value) {
-    return defaultValue;
-  }
-  const int = parseInt(value);
-  return isNaN(int) ? defaultValue : int;
-};
-
 async function initialize() {
   const databaseName = config.adapter.params.name || 'mssql';
-  const encryptDb = config.adapter.params.encrypt || false;
-  const dbSchema = config.adapter.params.schema || 'directories';
   const packetSize = config.adapter.params.packetSize || 32768;
 
   assert(config.adapter.params.username, 'Database property username must be supplied');
@@ -78,22 +68,16 @@ async function initialize() {
   this.db.userLegacyUsername = require('./userLegacyUsername.model')(sequelize);
   this.db.passwordHistory = require('./passwordHistory.model')(sequelize);
   this.db.userPasswordHistory = require('./userPasswordHistory.model')(sequelize);
-  this.db.userDevice = require('./userDevice.model')(sequelize);
   this.db.invitation = require('./invitation.model')(sequelize);
   this.db.invitationCallback = require('./invitationCallback.model')(sequelize);
-  this.db.invitationDevice = require('./invitationDevice.model')(sequelize);
   // define associations?
   // db.user.belongsTo(db.userPasswordPolicy, { sourceKey: 'sub' });
   this.db.userPasswordPolicy.belongsTo(this.db.user, { foreignKey: 'uid', sourceKey: 'sub', as: 'user' });
   this.db.user.hasMany(this.db.userLegacyUsername, { foreignKey: 'uid', sourceKey: 'sub' });
   this.db.user.belongsToMany(this.db.passwordHistory, { through: this.db.userPasswordHistory });
   this.db.passwordHistory.belongsToMany(this.db.user, { through: this.db.userPasswordHistory });
-  this.db.user.hasMany(this.db.userDevice, { foreignKey: 'uid', sourceKey: 'sub' });
-  this.db.userDevice.belongsTo(this.db.user, { foreignKey: 'uid', sourceKey: 'sub', as: 'user' });
-  this.db.invitation.hasMany(this.db.invitationDevice, { foreignKey: 'invitationId', sourceKey: 'id', as: 'devices' });
   this.db.invitation.hasMany(this.db.invitationCallback, { foreignKey: 'invitationId', sourceKey: 'id', as: 'callbacks' });
-  this.db.invitationDevice.belongsTo(this.db.invitation, { as: 'invitation' });
-  
+
   // sync all models with database
   await sequelize.sync({ alter: false, force: false });
 }

325fe42a12a0bc4ff50e30bb40381edb4febf4cb
Aroosa <aroosa.ali@education.gov.uk>
2026-02-12 00:26:39 +0000

    feature: added logic to update notifications when users are activated and deactivated

--- src/app/user/api/activateUser.js
+++ src/app/user/api/activateUser.js
@@ -1,5 +1,22 @@
+const { ServiceNotificationsClient } = require("login.dfe.jobs-client");
 const userAdapter = require("./../adapter");
 const logger = require("./../../../infrastructure/logger");
+const { safeUser } = require("../../../utils");
+const config = require("../../../infrastructure/config");
+
+const sendNotification = async (user, correlationId) => {
+    const serviceNotificationsClient = new ServiceNotificationsClient({
+      connectionString: config.notifications.connectionString,
+    });
+    const jobId = await serviceNotificationsClient.notifyUserUpdated(
+      safeUser(user),
+    );
+    logger.info(
+      `Send user updated notification for ${user.sub} with job id ${jobId} (reason: activate)`,
+      { correlationId },
+    );
+  };
+
 
 const changeStatus = async (req, res) => {
   try {
@@ -7,14 +24,16 @@ const changeStatus = async (req, res) => {
       return res.status(400).send();
     }
 
+    const correlationId = req.header("x-correlation-id");
     const user = await userAdapter.changeStatus(
       req.params.id,
       1,
-      req.header("x-correlation-id"),
+      correlationId,
     );
     if (!user) {
       return res.status(404).send();
     }
+    await sendNotification(user, correlationId);
     return res.send(true);
   } catch (e) {
     logger.error(e);
--- src/app/user/api/deactivateUser.js
+++ src/app/user/api/deactivateUser.js
@@ -1,5 +1,21 @@
+const { ServiceNotificationsClient } = require("login.dfe.jobs-client");
 const userAdapter = require("./../adapter");
 const logger = require("./../../../infrastructure/logger");
+const { safeUser } = require("../../../utils");
+const config = require("../../../infrastructure/config");
+
+const sendNotification = async (user, correlationId) => {
+    const serviceNotificationsClient = new ServiceNotificationsClient({
+      connectionString: config.notifications.connectionString,
+    });
+    const jobId = await serviceNotificationsClient.notifyUserUpdated(
+      safeUser(user),
+    );
+    logger.info(
+      `Send user updated notification for ${user.sub} with job id ${jobId} (reason: deactivate)`,
+      { correlationId },
+    );
+  };
 
 const changeStatus = async (req, res) => {
   try {
@@ -26,6 +42,7 @@ const changeStatus = async (req, res) => {
         correlation_id,
       );
     }
+    await sendNotification(user, correlation_id);
     return res.send(true);
   } catch (e) {
     logger.error(e);
